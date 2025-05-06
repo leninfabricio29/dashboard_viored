9fe7b5517161460ad04a19f15df39e6cb9ded654
leninfabricio29 <leninfabricio7@gmail.com>
2025-05-05 23:36:59 -0500

    Se corrigió el base URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 // Creamos una instancia de axios con la URL base
 const api = axios.create({
-  //baseURL: 'https://softkilla.es',
-  baseURL: 'http://localhost:5050',
+  baseURL: 'https://softkilla.es',
+ /// baseURL: 'http://localhost:5050',
   headers: {
     'Content-Type': 'application/json',
   },
